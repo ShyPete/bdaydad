5ff052a913753a48e29497e587120ecd92d9c957
BillyBurkley4550 <bobafettnoah9@gmail.com>
2026-01-13 21:19:33 -0500

    Capitalize Birthday and add hover color

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,21 @@
 import styles from "./page.module.css";
 
 export default function Home() {
+  const title = "Dad Birthday";
+
   return (
     <main className={styles.main}>
       <div className={styles.center}>
-        <h1>Dad birthday</h1>
+        <h1 className={styles.title}>
+          {title.split("").map((char, index) => (
+            <span
+              key={`${char}-${index}`}
+              className={char === " " ? styles.space : styles.letter}
+            >
+              {char === " " ? "\u00A0" : char}
+            </span>
+          ))}
+        </h1>
       </div>
     </main>
   );
